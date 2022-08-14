605dad587081f66402d847b1fce93467f32d56f7
Brandon Marrero <60243889+branmar97@users.noreply.github.com>
2022-08-14 08:52:21 -0400

    display sightings

--- app/routes/sightings.tsx
+++ app/routes/sightings.tsx
@@ -1,12 +1,44 @@
-import { Outlet } from "@remix-run/react";
+import { Outlet, Link, useLoaderData } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Sighting } from "@prisma/client";
+import { db } from "~/utils/db.server";
+
+type LoaderData = {
+    sightingListItems: Array<Sighting>;
+};
+
+export const loader: LoaderFunction = async () => {
+    const data: LoaderData = {
+        sightingListItems: await db.sighting.findMany(),
+    };
+    return json(data);
+}
 
 export default function SightingsRoute() {
-  return (
+    const data = useLoaderData<LoaderData>();
+
+    return (
     <div>
-      <h1>Bigfoot Sightings</h1>
-      <main>
-        <Outlet />
-      </main>
+        <header>
+            <h1>Bigfoot Sightings</h1>
+        </header>
+        
+        <main>
+            <div className="container">
+                <div className="sightings-list">
+                    <p>Recent reports of Bigfoot sightings</p>
+                    <ul>
+                        {data.sightingListItems.map((sighting) => (
+                            <li key={sighting.id}>
+                                <Link to={sighting.id}>{sighting.occurence}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <Outlet />
+            </div>
+        </main>
     </div>
-  );
+    );
 }
