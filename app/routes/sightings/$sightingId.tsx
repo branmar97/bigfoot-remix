31059ade83e5140d1d17a6f57063056189ddf281
Brandon Marrero <60243889+branmar97@users.noreply.github.com>
2022-08-14 16:59:37 -0400

    add single sighting data

--- app/routes/sightings/$sightingId.tsx
+++ app/routes/sightings/$sightingId.tsx
@@ -1,10 +1,41 @@
-export default function SightingRoute() {
-    return (
-      <div>
-        <p>A sighting</p>
-        <p>
-          Details of the sighting
-        </p>
-      </div>
-    );
+import type { Sighting } from "@prisma/client";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+type LoaderData = { sighting: Sighting };
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const sighting = await db.sighting.findUnique({
+    where: { id: params.sightingId }
+  })
+
+  if (!sighting) {
+    throw new Error("Record not found")
   }
+  
+  const data: LoaderData = { sighting };
+  return json(data)
+};
+
+export default function SightingRoute() {
+  const data = useLoaderData<LoaderData>();
+  
+  return (
+    <div>
+      <h2>{data.sighting.title}</h2>
+      <div>
+        <ul>
+          <li>Occured {data.sighting.occurence}</li>
+          <li>{data.sighting.city}, {data.sighting.state}</li>
+          <li>Vicinity: {data.sighting.vicinity}</li>
+          <li>Witnesses: {data.sighting.witnesses}</li>
+          <li>Conditions: {data.sighting.conditions}</li>
+          <li>Evidence: {data.sighting.evidence}</li>
+          <li>Details: {data.sighting.details}</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
